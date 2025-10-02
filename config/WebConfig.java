133f1c9852733171af3e1b9f973ff190c41a1330
shun <shun.soccer.shun@gmail.com>
2025-10-03 00:40:59 +0900

    CORS設定追加

---
+++ config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.bookpractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // すべてのAPIを対象
+                        .allowedOrigins("https://java-practice-book-management.vercel.app/")  // すべてのオリジンを許可（本番は限定するのが推奨）
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
